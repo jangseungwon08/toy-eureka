7fee99a4bec617e0278b2f0c8dfd0ddbd0542adc
jangseungwon08 <jangseungwon9819@gmail.com>
2025-12-17 17:13:00 +0900

    initial-commit

---
+++ src/test/java/com/toy/toy_eureka/ToyEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.toy_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
